66142f9dc412a011f760446afea5df51305e9857
Fallingstash <sashkaprog228@gmail.com>
2025-05-13 22:54:38 +0700

    upgrade checkpointManager for enemies

--- CheckPointManager.cs
+++ CheckPointManager.cs
@@ -1,22 +1,50 @@
+using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class CheckPointManager : MonoBehaviour
-{
-  private PlayerMemento lastCheckpoint;
+public class CheckPointManager : MonoBehaviour {
+  private PlayerMemento playerMemento;
+  private EnemyMemento[] enemyMemento;
   private PlayerController player;
+  private EnemyCore[] enemies;
 
   void Start() {
     player = FindAnyObjectByType<PlayerController>();
+    // Ищем ВСЕХ врагов, включая деактивированных
+    enemies = FindObjectsOfType<EnemyCore>(true);
+    enemyMemento = new EnemyMemento[enemies.Length];
   }
 
   public void SaveCheckpoint() {
-    lastCheckpoint = player.SaveState();
-    Debug.Log("Checkpoint saved at: " + lastCheckpoint.position);
+    // Сохраняем игрока
+    playerMemento = player.SaveState();
+
+    int count = 0;
+    for (int countOfEnemies = 0; countOfEnemies < enemies.Length; ++countOfEnemies) {
+      enemyMemento[count] = enemies[count].SaveState();
+      ++count;
+    }
+
+    Debug.Log("Checkpoint saved!");
   }
 
   public void LoadCheckpoint() {
-    if (lastCheckpoint != null) {
-      player.RestoreState(lastCheckpoint);
+    player.RestoreState(playerMemento);
+
+    enemies = FindObjectsOfType<EnemyCore>(true);
+
+    for (int countOfEnemies = 0; countOfEnemies < enemies.Length; ++countOfEnemies) {
+      if (enemies[countOfEnemies] == null) {
+        GameObject newEnemy = Instantiate(enemyMemento[countOfEnemies].enemyPref,
+                                        enemyMemento[countOfEnemies].position,
+                                        Quaternion.identity);
+        newEnemy.GetComponent<EnemyCore>().RestoreState(enemyMemento[countOfEnemies]);
+      }
+      else {
+        enemies[countOfEnemies].RestoreState(enemyMemento[countOfEnemies]);
+      }
     }
+
+    Time.timeScale = 1;
   }
 }
